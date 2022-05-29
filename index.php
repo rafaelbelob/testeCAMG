570f615c6570c104c99475abde93c919a02ab4e6
Rafael <rafaelbelob@gmail.com>
2022-05-29 15:43:44 -0300

    TesteCamg

---
+++ index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Teste na Cidade Administrativa(CRUD)</title>
+  <link rel="icon" type="image/png" href="IMG/BRASÃO-PRINCIPAL.png">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<style>
+  body{
+  border: 1px solid;
+  background-image: url(logolc.png);
+  background-repeat: no-repeat;
+  background-position: bottom right;
+  }
+</style>
+<div class="p-3 mb-2 bg-dark text-white bg-opacity-75">
+<body>
+<nav class="navbar bg-light hstack gap-3 ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">
+      <img src="IMG/home.jpg" alt="Inicio" width="30" height="24" class="d-inline-block align-text-top">
+      Ínicio
+    </a>
+    <a class="navbar-brand" href="?page=novo">
+      <img src="IMG/add.jpg" alt="Novo Usuario" width="30" height="24" class="d-inline-block align-text-top">
+      Adicionar Usuário
+    </a>
+    <a class="navbar-brand" href="?page=listar">
+      <img src="IMG/listar.jpg" alt="Inicio" width="30" height="24" class="d-inline-block align-text-top">
+      Listar Usuários
+    </a>
+  </div>
+</nav>
+  
+
+  <div class="container">
+    <div class="row">
+      <div class="col mt-5">
+
+        <?php
+
+        include("config.php");
+        switch (@$_REQUEST["page"]) {
+          case "novo":
+            include("novo-usuario.php");
+            break;
+          case "listar":
+            include("listar-usuario.php");
+            break;
+          case "salvar":
+            include("salvar-usuario.php");
+            break;
+          case "editar":
+            include("editar-usuario.php");
+            break;
+          default:
+            print "<h1>Bem vindos !</h1>";
+        }
+        ?>
+
+      </div>
+    </div>
+  </div>
+      </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+<footer>
+<div class="rounded float-none ">
+  <img src="IMG/bb.jpg" class="fixed-bottom rounded float-none" title="© BB" width="100px">
+</div>
+</footer>
+
+</html>
---
+++ listar-usuario.php
@@ -0,0 +1,40 @@
+<h1>Listar Usuarios</h1>
+<?php
+
+$sql = "SELECT * FROM usuarios";
+
+$res = $conn->query($sql);
+$qtd = $res->num_rows;
+if ($qtd > 0) {
+    print "<table class='table table-hover table-striped table-bordered text-light'>";
+    print "<tr>";
+    print "<th class='text-light'> # </th>";
+    print "<th class='text-light'>Nome</th>";
+    print "<th class='text-light'>E-Mail</th>";
+    print "<th class='text-light'>Data de Nascimento</th>";
+    print "<th class='text-light'>Ações</th>";
+    print "</tr>";
+
+    while ($row = $res->fetch_object()) {
+        print "<tr>";
+        print "<td>" . $row->id . "</td>";
+        print "<td>" . $row->nome . "</td>";
+        print "<td>" . $row->email . "</td>";
+        print "<td>" . $row->data_nasc . "</td>";
+
+        print "<td>
+        <button onclick=\"location.href='?page=editar&id=" . $row->id . "';\" class ='btn btn-success'>Editar</button>
+        
+        <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar&acao=excluir&id=" . $row->id . "';}
+        else{false;}\" class ='btn btn-danger'>Excluir</button>
+        </td>";
+
+        print "</tr>";
+    }
+} else {
+    print "<p class = alert alert-danger'>Não encontrou resultado!</p>";
+}
+
+
+
+?>
